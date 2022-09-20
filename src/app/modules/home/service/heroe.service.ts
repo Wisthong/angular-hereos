08ff1faa040e5065867b1443d5a0df81a05d2340
Wisthong Martinez <wisthongd@gmail.com>
2022-09-19 22:01:30 -0500

    Variables de entorno habilitadas para deploy

--- src/app/modules/home/service/heroe.service.ts
+++ src/app/modules/home/service/heroe.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { Observable } from 'rxjs';
 import { Heroe } from '../models/heroe';
 
@@ -8,6 +8,7 @@ import { Heroe } from '../models/heroe';
   providedIn: 'root',
 })
 export class HeroeService {
+
   private readonly API = environment.api;
   constructor(private readonly http: HttpClient) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://api-heroes-c.herokuapp.com',
 };
